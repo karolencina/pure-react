f893d0c35e1c8be7b06b63a1cdf457e702a73c03
Karolina Lencina <karolencina@icloud.com>
2022-12-24 10:24:21 +0100

    Create Avatar component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -6,4 +6,14 @@ function Tweet() {
     return <div className="tweet">Tweet</div>
 }
 
+function Avatar() {
+    return(
+    <img
+        src="https://www.gravatar.com/avatar/f82bdf2f2ddf1e2f0998671c24991435"
+        className="avatar"
+        alt="avatar"
+    />
+)
+}
+
 ReactDOM.render(<Tweet/>, document.querySelector("#root"))
