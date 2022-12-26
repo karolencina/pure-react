d6f74e41fa0e741a04bc390811c7113dc06799bc
Karolina Lencina <karolencina@icloud.com>
2022-12-26 23:14:08 +0000

    Create message component

--- static-tweet/src/index.js
+++ static-tweet/src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 function Tweet() {
-    return <div className="tweet">Tweet</div>
+    return <div className="tweet">
+        <Avatar></Avatar>
+        <Message></Message>
+    </div>
 }
 
 function Avatar() {
@@ -14,6 +17,10 @@ function Avatar() {
         alt="avatar"
     />
 )
+}
+
+function Message() {
+    return <div className="message">This is the content of a Tweet message.</div>
 }
 
 ReactDOM.render(<Tweet/>, document.querySelector("#root"))
